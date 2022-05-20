43c8a3ba9b8b239a87cccb0b5c7b70a3919c4810
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-20 20:30:10 +0800

    debugging

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
 // BOUNCING BALLS - defining variables
+let ballCount = 100;
+let x = [];
+let y = [];
 let xSpeed = [];
 let ySpeed = [];
 let r = [];
@@ -25,7 +28,7 @@ function setup() {
     set the speeds to be random
     set the size to be random
     set the colors to be random
-    
+    */
     for(let i = 0; i < ballCount; i++) {
         x[i] = width / 2;
         y[i] = height / 2;
@@ -36,49 +39,46 @@ function setup() {
         g[i] = random(0, 256);
         b[i] = random(0, 256);
     }
-    */
+    
+    // AUDIOVISUALIZER
     // fast fourier transform object to return array of values for each point of time analyzed
     //fft = new p5.FFT()
 
 
 }
 
-// p5js function draw. this works. 
+// P5JS FUNCTION DRAW. this works. 
 function draw() {
     background(0)
+    // ORANGE TEST BALL 
     noStroke();
     fill('#FF8C00')
     ellipse(width/2, height/2, 100,100)
   
-    // TODO: Iterate through a new for loop to change the properties in order to animate the balls
-  for(let i = 0; i< ballCount; i++){
-    // Inside the for loop:
-    // TODO: Increment speed for x position
-      x[i] += xSpeed[i];
-
-    // TODO: Increment speed for y position
-      y[i] += ySpeed[i];
-    
-    // TODO: Reverse x direction if ball hits left or right sides
-    if(x[i] < 0 || x[i] > width) {
-      xSpeed[i] *= -1;
-    }
-
-    // TODO: Reverse y direction if ball hits top or bottom sides
-    if(y[i] < 0 || y[i] > width) {
-      ySpeed[i] *= -1;
-    }
-
-    // TODO: Set random R, G, B values
+    /*Iterate through a new for loop to change the properties in order to animate the balls
+    Inside the for loop:
+    increment the speed for x position, y position;
+    reverse x direction if ball hits left or right sides,
+    reverse y direction if ball hits top or bottom sides;
+    set random R, G, B values,
+    style to have no strokes,
+    draw balls
+    */
+   for(let i = 0; i < ballCount; i++) {
+    x[i] += xSpeed[i];
+    y[i] += ySpeed[i];
+  if(x[i] < 0 || x[i] > width) {
+    xSpeed[i] *= -1;
+  }
+  if(y[i] < 0 || y[i] > width) {
+    ySpeed[i] *= -1;
+  }
     fill(r[i], g[i], b[i]);
-
-    // TODO: Style to have no strokes
     noStroke();
-
-    // TODO: Draw the bouncing balls
     ellipse(x[i], y[i], size[i], size[i]);
   }
 
+  // AUDIOVISUALIZER
     // create variable to store waveform data from fft. calling fft.waveform returns an array with 1024 elements in chrome dev tools
     var wave = fft.waveform()
 
@@ -93,7 +93,7 @@ function draw() {
     }
 }
 
-// play song
+// AUDIO CONTROLS
 function mousePressed() {
     userStartAudio();
 }
