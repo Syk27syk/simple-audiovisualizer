4fdc40268782b3c2bea6f7df7e54d9e146888e1d
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-20 15:10:28 +0800

    tailwind config, tentative with template paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./script.js"
+  ],
   theme: {
+    fontFamily: {
+      'oswald': ['oswald', 'sans-serif'],
+      'lilita': ['"Lilita One"', 'sans-serif'],
+      'roboto': ['Roboto', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
