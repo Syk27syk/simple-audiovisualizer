40526ada3ec33b3c31c022b626f6ef0d6d2a4cfd
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-20 22:10:51 +0800

    Audio visualizer - circle

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+const { LoopOnce } = require("three");
+const { NoColorSpace } = require("three");
+
 // TEST BALLS
 let aPos = 0;
 let aSpeed = 5;
@@ -50,7 +53,7 @@ function setup() {
     
     // AUDIOVISUALIZER
     // fast fourier transform object to return array of values for each point of time analyzed
-    //fft = new p5.FFT()
+    fft = new p5.FFT()
 
 }
 
@@ -114,21 +117,65 @@ function draw() {
 
   // AUDIOVISUALIZER
     // create variable to store waveform data from fft. calling fft.waveform returns an array with 1024 elements in chrome dev tools
+    stroke(255)
+    noFill()
+    translate(width / 2, height / 2)
     var wave = fft.waveform()
 
+    // the first line is only for the full circle
+    for (var t = -1; t <= 1; t +=2) {
+        beginShape()
+        // i++ works too. i += 0.5 makes it more complex. 
+        for (var i = 0; i <= 180; i += 0.5) {
+            var index = floor(map(i, 0, 180, 0, wave.length -1))
+            var r = map(wave[index], -1, 1, 150, 350)
+            var x = r + sin(i) * t
+            var y = r + cos(i)
+            vertex (x,y)
+        }
+        endShape()
+    }
+    /* this is for a line waveform: 
     for (var i = 0; i < width; i++) {
         var index = floor(map(i, 0, width, 0, wave.length))
-        var r = map(wave[index], -1, 1, 150, 150)
-        //y = wave[index] = 300 + height / 2
+        var x = i
+        var y = wave[index] * 300 + height / 2
+        point (x,y)
+    */
+    /*The following is the semicircle, it should be in degrees, not radians. 
+    for (var i = 0; i <= 180; i++) {
+        var index = floor(map(i, 0, 180, 0, wave.length -1))
+        var r = map(wave[index], -1, 1, 150, 350)
         var x = r + sin(i)
         var y = r + cos(i)
         vertex (x,y)
-       //point (x,y)
     }
+    endShape()
+    */
+    /*this is the other half of the circle. note the minus in front of the sine function. 
+    beginShape()
+     for (var i = 0; i <= 180; i++) {
+        var index = floor(map(i, 0, 180, 0, wave.length -1))
+        var r = map(wave[index], -1, 1, 150, 350)
+        var x = r + -sin(i)
+        var y = r + cos(i)
+        vertex (x,y)
+    }
+    endShape()
+    */
+
 }
 
 // AUDIO CONTROLS
 function mousePressed() {
     userStartAudio();
+    if (song.isPlaying()) {
+        song.pause()
+        noLoop()
+        //loop and noloop makes soundwave freeze when audio pauses
+    } else {
+        song.play() 
+        loop()
+    }
 }
 
